a50f4fbc45d206fc963cd335eb5fe9379f78ea82
Martin Siron <msiron@lbl.gov>
2019-07-22 10:35:20 -0700

    fix imports

--- atomate/vasp/fireworks/absorption.py
+++ atomate/vasp/fireworks/absorption.py
@@ -67,7 +67,7 @@ class AbsorptionEnergyLandscapeFW(Firework):
                  vasp_cmd=VASP_CMD, prev_calc_loc=True, prev_calc_dir=None, db_file=DB_FILE, vasptodb_kwargs=None,
                  parents=None, contcar_to_poscar = True,**kwargs):
         """
-        Copied from StaticFW
+        Copied from StaticFW to be modified with the addition of a task
         Standard static calculation Firework - either from a previous location or from a structure.
 
         Args:
@@ -112,3 +112,59 @@ class AbsorptionEnergyLandscapeFW(Firework):
         t.append(
             VaspToDb(db_file=db_file, **vasptodb_kwargs))
         super(AbsorptionEnergyLandscapeFW, self).__init__(t, parents=parents, name=fw_name, **kwargs)
+
+class AdsorptionOptimizeFW(Firework):
+
+    def __init__(self, structure, name="structure optimization",
+                 vasp_input_set=None,
+                 vasp_cmd=VASP_CMD, override_default_vasp_params=None,
+                 ediffg=None, db_file=DB_FILE, 
+                 force_gamma=True, job_type="double_relaxation_run",
+                 max_force_threshold=RELAX_MAX_FORCE,
+                 auto_npar=">>auto_npar<<",
+                 half_kpts_first_relax=HALF_KPOINTS_FIRST_RELAX, parents=None,
+                 vasptodb_kwargs= None,**kwargs):
+        """
+        Copied from OptimizeFW - removed parent
+        Optimize the given structure.
+
+        Args:
+            structure (Structure): Input structure.
+            name (str): Name for the Firework.
+            vasp_input_set (VaspInputSet): input set to use. Defaults to MPRelaxSet() if None.
+            override_default_vasp_params (dict): If this is not None, these params are passed to 
+                the default vasp_input_set, i.e., MPRelaxSet. This allows one to easily override 
+                some settings, e.g., user_incar_settings, etc.
+            vasp_cmd (str): Command to run vasp.
+            ediffg (float): Shortcut to set ediffg in certain jobs
+            db_file (str): Path to file specifying db credentials to place output parsing.
+            force_gamma (bool): Force gamma centered kpoint generation
+            job_type (str): custodian job type (default "double_relaxation_run")
+            max_force_threshold (float): max force on a site allowed at end; otherwise, reject job
+            auto_npar (bool or str): whether to set auto_npar. defaults to env_chk: ">>auto_npar<<"
+            half_kpts_first_relax (bool): whether to use half the kpoints for the first relaxation
+            parents ([Firework]): Parents of this particular Firework.
+            vasptodb_kwargs: Passed to VaspToDB firetask for custom tasks to be passed on. 
+                Useful to add "task_fields_to_push" parameter
+            \*\*kwargs: Other kwargs that are passed to Firework.__init__.
+        """
+        override_default_vasp_params = override_default_vasp_params or {}
+        vasp_input_set = vasp_input_set or MPRelaxSet(structure,
+                                                      force_gamma=force_gamma,
+                                                      **override_default_vasp_params)
+
+        t = []
+        t.append(WriteVaspFromIOSet(structure=structure,
+                                    vasp_input_set=vasp_input_set))
+        t.append(RunVaspCustodian(vasp_cmd=vasp_cmd, job_type=job_type,
+                                  max_force_threshold=max_force_threshold,
+                                  ediffg=ediffg,
+                                  auto_npar=auto_npar,
+                                  half_kpts_first_relax=half_kpts_first_relax))
+        t.append(PassCalcLocs(name=name))
+        t.append(
+            VaspToDb(db_file=db_file, additional_fields={"task_label": name}, **vasptodb_kwargs))
+        super(AdsorptionOptimizeFW, self).__init__(t, parents=parents, name="{}-{}".
+                                         format(
+                                             structure.composition.reduced_formula, name),
+                                         **kwargs)
